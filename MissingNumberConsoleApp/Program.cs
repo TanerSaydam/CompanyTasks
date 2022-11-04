5c92f9c1064bbae72ff5e74c9a6747f3d772118e
taner <taner@DESKTOP-3BJ5GK9>
2022-11-04 10:30:40 +0300

    .

---
+++ MissingNumberConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> numbers = new() { 1, 3, 11, 13, 20, 37 };
+
+List<int> newNumbers = new();
+
+for (int i = 0; i < numbers.Count(); i++)
+{
+    if (numbers.Count() > i + 1)
+    {
+        var r = numbers[i + 1] - numbers[i];
+        for (int x = 1; x <= r; x++)
+        {
+            var y = numbers[i] + x;
+            if (y % 2 != 0 && numbers[i + 1] != y)
+                newNumbers.Add(y);
+        }
+    }
+}
+
+foreach (var n in newNumbers)
+{
+    Console.WriteLine(n);
+}
